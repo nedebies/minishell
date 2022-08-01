3b882b05657f1134284f7badd8b5b64b2b4ef751
Maja Kapturska <majakapturska@MacBook-Pro-de-Maja.local>
2022-08-01 13:36:43 +0200

    ajout premier .h et main

---
+++ minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 13:30:29 by nedebies          #+#    #+#             */
+/*   Updated: 2022/08/01 13:35:41 by nedebies         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+
+#endif
