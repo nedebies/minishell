441985c34c274d84ba576bc766ed1e0d4654a961
Nestor Debiesme <nedebies@fu-r2-p4.s19.be>
2022-08-02 13:42:34 +0200

    ajout macro pour un prompt colore plus visible

--- minishell.h
+++ minishell.h
@@ -6,16 +6,18 @@
 /*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 13:30:29 by nedebies          #+#    #+#             */
-/*   Updated: 2022/08/01 13:35:41 by nedebies         ###   ########.fr       */
+/*   Updated: 2022/08/02 13:32:07 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#define GRN "\e[1;32m"
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
 # include <stdio.h>
-
-
+# include <readline/readline.h>
+# include <readline/history.h>
 #endif
