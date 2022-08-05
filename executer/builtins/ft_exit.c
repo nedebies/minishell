eeac57f0b86f872669427bd35a4400cc564c7ce9
Nestor Debiesme <nedebies@fu-r3-p1.s19.be>
2022-08-05 12:49:29 +0200

    implementation d'une fonction exit sommaire

--- executer/builtins/ft_exit.c
+++ executer/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:41:01 by nedebies          #+#    #+#             */
-/*   Updated: 2022/08/05 12:45:46 by nedebies         ###   ########.fr       */
+/*   Updated: 2022/08/05 12:48:39 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 int ft_exit(char *str)
 {
-    (void)str;
-    return (0);
+    int     i;
+    int     j;
+    char    *exit;
+
+    i = 0;
+    j = 0;
+    exit = "exit";
+    if (str)
+    {
+        while (str[j] == 32)
+		    j++;
+        while(exit[i])
+        {
+            if (str[i + j] != exit[i])
+                return (0);
+            i++;
+        }
+    }
+    return (1);
 }
--- main.c
+++ main.c
@@ -6,34 +6,11 @@
 /*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 13:27:18 by nedebies          #+#    #+#             */
-/*   Updated: 2022/08/05 12:44:29 by nedebies         ###   ########.fr       */
+/*   Updated: 2022/08/05 12:48:51 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-/**
-static int ft_exit(char *str)
-{
-    int     i;
-    int     j;
-    char    *exit;
-
-    i = 0;
-    j = 0;
-    exit = "exit";
-    if (str)
-    {
-        while (str[j] == 32)
-		    j++;
-        while(exit[i])
-        {
-            if (str[i + j] != exit[i])
-                return (0);
-            i++;
-        }
-    }
-    return (1);
-}**/
 
 static int ft_parse_builtins(char *str)
 {
