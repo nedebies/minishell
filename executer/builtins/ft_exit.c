ac25911f6b3d46293edd631a60095271602a24af
Maja Kapturska <majakapturska@MacBook-Pro-de-Maja.local>
2022-08-09 23:28:35 +0200

    norme main + upgrade exit

--- executer/builtins/ft_exit.c
+++ executer/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 12:41:01 by nedebies          #+#    #+#             */
-/*   Updated: 2022/08/08 13:53:07 by nedebies         ###   ########.fr       */
+/*   Updated: 2022/08/09 23:26:17 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 int ft_exit(char **split)
 {
-    if (!ft_strncmp(split[0], "exit", 4))
+    if (!ft_strncmp(split[0], "exit", 4)) //erreur si split[2] existe
+    {
+        ft_putstr_fd("exit\n", 1);
         return (1);
+    }
     return (0);
 }
--- main.c
+++ main.c
@@ -6,64 +6,64 @@
 /*   By: nedebies <nedebies@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 13:27:18 by nedebies          #+#    #+#             */
-/*   Updated: 2022/08/09 23:22:13 by nedebies         ###   ########.fr       */
+/*   Updated: 2022/08/09 23:23:57 by nedebies         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
- static	int	ft_free_split(char **split)
- {
-     size_t i;
-     i = 0;
-     while (split[i])
-         i++;
- 	while (i != 0)
- 		free(split[--i]);
- 	free(split);
- 	return (0);
- }
+static	int	ft_free_split(char **split)
+{
+	size_t i;
+	i = 0;
+	while (split[i])
+		i++;
+	while (i != 0)
+		free(split[--i]);
+	free(split);
+	return (0);
+}
 
- static int ft_parse_builtins(char **split, char **envp)
- {
-     if(ft_cd(split))
-         return(0);
-     if(ft_echo(split))
-         return(0);
-     if(ft_env(split, envp))
-         return(0);
-     if(ft_exit(split))
-         return(1);
-     if(ft_export(split))
-         return(0);
-     if(ft_pwd(split, envp))
-         return(0);
-     if(ft_unset(split))
-         return(0);
-     return (0);
- }
+static int ft_parse_builtins(char **split, char **envp)
+{
+	if(ft_cd(split))
+		return(0);
+	if(ft_echo(split))
+		return(0);
+	if(ft_env(split, envp))
+		return(0);
+	if(ft_exit(split))
+		return(1);
+	if(ft_export(split))
+		return(0);
+	if(ft_pwd(split, envp))
+		return(0);
+	if(ft_unset(split))
+		return(0);
+	return (0);
+}
 
 int main(int ac, char **av, char **envp)
 {
-    char *str; // readline()
-    char **cmd; // readline() splitted and TOKENIZED
-    (void)av;
+	char *str; // readline()
+	char **cmd; // readline() splitted and TOKENIZED
 
-    str = NULL;
-    cmd = NULL;
-    while (ac > 0)
-    {
-        str = readline(GRN"not-bash&> "GRN);
-        add_history(str); //simplier to create an history than a list
-        cmd = lexer(str);
-        //cmd = ft_split(str, 32);
-        if (!cmd || ft_parse_builtins(cmd, envp))
-        {
-            if (cmd)
-                ft_free_split(cmd);
-            break ;
-        }  // NO CTRL+C CTRL+D CTRL+(backslash) atm
-    }
-    free(str);
-    return (0);
+	(void)av;
+	str = NULL;
+	cmd = NULL;
+	while (ac > 0)
+	{
+		str = readline(GRN"not-bash&> "GRN);
+		add_history(str); //simplier to create an history than a list
+		cmd = lexer(str);
+		//cmd = ft_split(str, 32);
+		if (!cmd || ft_parse_builtins(cmd, envp))
+		{
+			if (cmd)
+				ft_free_split(cmd);
+			break ;
+		}  // NO CTRL+C CTRL+D CTRL+(backslash) atm
+	}
+	free(str);
+	return (0);
 }
